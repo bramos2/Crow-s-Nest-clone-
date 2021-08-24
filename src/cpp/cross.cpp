6bd9f9b2efed0ee0399c727b4c67ab165a4d24ea
Conscat <wgooch2000@gmail.com>
2021-08-24 14:56:31 -0400

    Minor path optimization

--- src/cpp/cross.cpp
+++ src/cpp/cross.cpp
@@ -12,7 +12,7 @@ auto find_exe_path() -> std::string {
   std::array<char, MAX_PATH> result{};
   std::string full_path = std::string(
       result.data(), GetModuleFileName(NULL, result.data(), MAX_PATH));
-  return std::string(full_path.substr(0, full_path.find_last_of('\\\\'))) + "/";
+  return std::string(full_path.substr(0, full_path.find_last_of('\\\\')));
 }
 #else
 #include <linux/limits.h>
@@ -20,12 +20,13 @@ auto find_exe_path() -> std::string {
 auto find_exe_path() -> std::string {
   std::array<char, PATH_MAX - NAME_MAX> result{};
   std::string full_path = std::string(result.data());
-  return std::string(full_path.substr(0, full_path.find_last_of('/'))) + "/";
+  return std::string(full_path.substr(0, full_path.find_last_of('/')));
 }
 #endif
 
+// NOLINTNEXTLINE I usually want to pass this value directly.
 auto find_shader_path(std::string spv_name) -> string {
-  return crow::find_exe_path() + "../../res/spv/" + spv_name;
+  return crow::find_exe_path() + "/../../res/spv/" + spv_name;
 }
 
 }  // namespace crow
